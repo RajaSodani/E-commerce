78f653fc648c98627595704033c49d3afa6674da
Raja Sodani <148634774+RajaSodani@users.noreply.github.com>
2024-07-20 16:49:22 +0530

    Update shop.jsx

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -17,7 +17,7 @@ export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>PedroTech Shop</h1>
+        <h1>Techiie Shop</h1>
       </div>
 
       <SearchBar />
